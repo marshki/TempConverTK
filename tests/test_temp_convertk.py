1d9cf309bc2073fb2ae1ebf98d36d3f11609dd3d
marshki <marshall@morethantomorrow.org>
2024-04-14 09:50:55 -0400

    fix import order

--- tests/test_temp_convertk.py
+++ tests/test_temp_convertk.py
@@ -5,9 +5,10 @@
 import os
 import sys
 import unittest
-from temp_convertk import convert_f2c, convert_c2f
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from temp_convertk import convert_f2c, convert_c2f
 
 class TestTemperatureConversion(unittest.TestCase):
 
